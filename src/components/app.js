30bfc3236d8085a2a7c84583096d863d7e048c5d
RosaFraile <rosafraile@hotmail.com>
2024-06-28 16:22:14 +0200

    created the site pages and installed react-router-dom to configure the routes of the site

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route 
+} from "react-router-dom";
+
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import WritePost from "./pages/writePost";
+import PostDetail from "./pages/postDetail";
+import NoMatch from "./pages/noMatch";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <Router>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register} />
+                <Route path="/write" component={WritePost} />
+                <Route path="/post/:id" component={PostDetail}/>
+                <Route component={NoMatch} />
+              </Switch>
+          </Router>
       </div>
     );
   }
